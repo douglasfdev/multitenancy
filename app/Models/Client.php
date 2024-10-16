cb37f3a174e0626de6623958b0ec05b40c31e9f2
Douglas Fernandes <douglas.fernandes91@live.com>
2024-10-16 10:47:30 -0300

    feat(company): make a scope to get companies by user seller logged

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\CompanyScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[ScopedBy([CompanyScope::class])]
 class Client extends Model
 {
     use HasFactory, SoftDeletes;
